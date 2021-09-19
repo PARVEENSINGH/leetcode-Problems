550d9364cccb6920cab11cbc2e42e9430f4f773c
PARVEENSINGH <parveenk8057@gmail.com>
2021-09-19 23:01:22 +0530

    maintaingingg Daily Count

---
+++ leetcode/SearchInsertPos.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0, r = nums.size(), mid = 0;
+        while (l<=r)
+        {
+            mid = (l+r)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>target)
+                l = mid+1;
+            else if(nums[mid]<target)
+                r = mid-1;        
+        }
+        return mid;
+        
+    }
+};
