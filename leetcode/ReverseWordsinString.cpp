ee3955073f899d805f2b66d0124339ffcc880414
PARVEENSINGH <parveenk8057@gmail.com>
2022-03-31 15:49:25 +0530

    Reverse words only in a string

---
+++ leetcode/ReverseWordsinString.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//https://leetcode.com/problems/reverse-words-in-a-string/
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n = s.length();
+        string res= "";
+        string temp = "";
+        for(int i = n-1;i>=0;i--){
+            temp = "";
+            if(s[i]==' ')
+                continue;
+            while(i>=0 && s[i]!=' '){
+                temp+=s[i];
+                i--;
+            }
+            if(temp!=""){
+                reverse(temp.begin(),temp.end());
+                if(i>0)
+                    res+=temp+' ';
+                if(i<=0)
+                     res+=temp;
+            }
+        }
+        int l = res.length()-1;
+        if(res[l]==' ')
+            l--;
+        return res.substr(0,l+1);
+    }
+};
